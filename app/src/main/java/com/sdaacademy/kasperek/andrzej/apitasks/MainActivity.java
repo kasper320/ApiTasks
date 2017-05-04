3a10beb49fe17cafb7f6a192e97c834ed6905aac
kasper320 <kasperek.andrzej@gmail.com>
2017-05-04 21:01:33 +0200

    Created adding new task.

--- app/src/main/java/com/sdaacademy/kasperek/andrzej/apitasks/MainActivity.java
+++ app/src/main/java/com/sdaacademy/kasperek/andrzej/apitasks/MainActivity.java
@@ -1,18 +1,31 @@
 package com.sdaacademy.kasperek.andrzej.apitasks;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.gson.Gson;
 import com.sdaacademy.kasperek.andrzej.apitasks.adapter.Adapter;
 import com.sdaacademy.kasperek.andrzej.apitasks.dto.TaskDTO;
 import com.sdaacademy.kasperek.andrzej.apitasks.dto.mapper.Mapper;
 import com.sdaacademy.kasperek.andrzej.apitasks.model.Task;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -37,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
     private List<TaskDTO> taskDTOList;
     private List<Task> taskList;
     Adapter adapter;
+    private TaskDTO taskDTO;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,15 +63,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         taskList = new ArrayList<>();
-        adapter = new Adapter(this,R.layout.single_row,taskList);
+        adapter = new Adapter(this, R.layout.single_row, taskList);
         show.setAdapter(adapter);
         new MyTask().execute();
-    }
-    @OnClick
-    public void onClick(View view){
-        new MyTask().execute();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 
     private class MyTask extends AsyncTask<Void, Void, TaskDTO[]> {
         private String url = "https://shrouded-fjord-81597.herokuapp.com/api/task/all/2";
@@ -75,4 +130,37 @@ public class MainActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
         }
     }
+
+    public class AddNewTask extends AsyncTask<Void, Void, TaskDTO> {
+
+        @Override
+        protected TaskDTO doInBackground(Void... params) {
+
+            RestTemplate restTemplate = new RestTemplate();
+            Gson gson = new Gson();
+            String url = "https://shrouded-fjord-81597.herokuapp.com/api/task";
+            String requestBody = gson.toJson(taskDTO);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<Object> entity = new HttpEntity<Object>(requestBody, headers);
+            ResponseEntity<TaskDTO> response = restTemplate.exchange(url, HttpMethod.PUT, entity, TaskDTO.class);
+            TaskDTO responseBody = response.getBody();
+
+            return responseBody;
+        }
+
+    }
+
+    @OnClick
+    public void onClick(View view) {
+        taskDTO = new TaskDTO();
+        taskDTO.setValue(dataEntryView.getText().toString());
+        taskDTO.setUser(2);
+        taskDTO.setCompleated(false);
+        new AddNewTask().execute();
+        taskList.clear();
+        new MyTask().execute();
+    }
+
+
 }
--- app/src/main/java/com/sdaacademy/kasperek/andrzej/apitasks/adapter/Adapter.java
+++ app/src/main/java/com/sdaacademy/kasperek/andrzej/apitasks/adapter/Adapter.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import com.sdaacademy.kasperek.andrzej.apitasks.MainActivity;
 import com.sdaacademy.kasperek.andrzej.apitasks.R;
 import com.sdaacademy.kasperek.andrzej.apitasks.dto.TaskDTO;
 import com.sdaacademy.kasperek.andrzej.apitasks.model.Task;
@@ -21,21 +23,39 @@ import static com.sdaacademy.kasperek.andrzej.apitasks.R.styleable.View;
  * Created by RENT on 2017-04-29.
  */
 
-public class Adapter extends ArrayAdapter<Task>{
+public class Adapter extends ArrayAdapter<Task> {
     public Adapter(Context context, int resource, List<Task> objects) {
         super(context, 0, objects);
     }
+
     @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        Task task = getItem(position);
-        if(convertView==null){
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Task task = getItem(position);
+        if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.single_row, parent, false);
+
+            CheckBox complete = (CheckBox) convertView.findViewById(R.id.checkBoxCompleted);
+            complete.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                private final Task task1 = task;
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (buttonView.isChecked()) {
+                        task1.setCompleated(true);
+                    } else {
+                        task1.setCompleated(false);
+                    }
+                    task1.setValue(task.getValue());
+                    task1.setId(task.getId());
+
+                }
+
+            });
         }
         TextView number = (TextView) convertView.findViewById(R.id.textViewTaskNumber);
         TextView value = (TextView) convertView.findViewById(R.id.textViewListItem);
-        CheckBox complete= (CheckBox) convertView.findViewById(R.id.checkBoxCompleted);
-        number.setText(String.valueOf(position));
+        CheckBox complete = (CheckBox) convertView.findViewById(R.id.checkBoxCompleted);
+        number.setText(String.valueOf(position + 1));
         value.setText(task.getValue());
         complete.setChecked(task.isCompleated());
         return convertView;
